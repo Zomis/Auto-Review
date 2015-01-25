93f56674529fa581a9edbc38eebc35c2c5f9b6c4
Simon Forsberg <zomis2k@hotmail.com>
2015-01-25 02:00:58 +0100

    improved positioning of checkboxes a bit

--- autoreview.js
+++ autoreview.js
@@ -31,17 +31,30 @@ embedFunction('showAutoreviewButtons', function() {
 	var line = "";
 	var first = null;
 	for (i = 0; i < count; i++) {
-		console.log(i);
 		var element = $(spans[ i ]);
 		
 		if (first === null) {
 			first = element;
 		}
 		if (element.text().indexOf("\n") !== -1) {
-			console.log( i + ": " + element.text() );
-			first.before('<input type="checkbox" data-line="' + line + '" />');
+			console.log(i + " line: " + line);
+			var lastBreak = element.text().lastIndexOf("\n");
+			var beforeBreak = element.text().substr(0, lastBreak);
+			var afterBreak = element.text().substr(lastBreak + 1);
+			element.text(beforeBreak + "\n");
+			var dataProperty = ""; // 'data-line="' + line + '" ';
+			var span = '<span class="pln zomis">' + afterBreak + '</span>';
+			console.log("clog befor " + beforeBreak);
+			console.log("clog after " + afterBreak);
+			if (afterBreak.length === 0) {
+				span = "";
+			}
+			first.before('<input type="checkbox" ></input>' + span);
 			first = null;
 			line = "";
+		}
+		else {
+			line += element.text();
 		}
 		
 	}
