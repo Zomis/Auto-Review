a4a43e04eca67bb15a0179f1f33ee2f922668b62
Andrew Elliott <aelliott@onatlas.com>
2020-04-21 16:26:50 -0700

    separate functions, use es-6 syntax, some functional approaches, more jQuery utilities

--- autoreview.js
+++ autoreview.js
@@ -1,115 +1,93 @@
 /** @preserve
-// ==UserScript==
-// @name          Auto-Review
-// @grant         none
-// @author        Simon Forsberg
-// @namespace     zomis
-// @homepage      https://www.github.com/Zomis/Auto-Review
-// @description	  Adds checkboxes for copying code in a post to an answer.
-// @include       http://stackoverflow.com/*
-// @include       http://meta.stackoverflow.com/*
-// @include       http://superuser.com/*
-// @include       http://serverfault.com/*
-// @include       http://meta.superuser.com/*
-// @include       http://meta.serverfault.com/*
-// @include       http://stackapps.com/*
-// @include       http://askubuntu.com/*
-// @include       http://*.stackexchange.com/*
-// @exclude       http://chat.stackexchange.com/*
-// ==/UserScript==
-*/
+ // ==UserScript==
+ // @name          Auto-Review
+ // @grant         none
+ // @author        Simon Forsberg
+ // @namespace     zomis
+ // @homepage      https://www.github.com/Zomis/Auto-Review
+ // @description	  Adds checkboxes for copying code in a post to an answer.
+ // @include       http://stackoverflow.com/*
+ // @include       http://meta.stackoverflow.com/*
+ // @include       http://superuser.com/*
+ // @include       http://serverfault.com/*
+ // @include       http://meta.superuser.com/*
+ // @include       http://meta.serverfault.com/*
+ // @include       http://stackapps.com/*
+ // @include       http://askubuntu.com/*
+ // @include       https://*.stackexchange.com/*
+ // @exclude       http://chat.stackexchange.com/*
+ // ==/UserScript==
+ */
 
-function embedFunction(name, theFunction) {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.textContent = theFunction.toString().replace(/function ?/, 'function ' + name);
-    document.getElementsByTagName('head')[0].appendChild(script);
-}
+const addCheckedLinesToAnswer = event => {
+	const answer = $("#wmd-input");
+	let answer_text = answer.val();
+	let added_lines = 0;
+	let added_blocks = 0;
 
-embedFunction('showAutoreviewButtons', function(clickedObject) {
-	
-	var i;
+	// loop through checkboxes and prepare answer
+	const checkboxes = $("input.autoreview:checked");
+	const block = [];
+	checkboxes.each((i, checkboxElement) => {
+		const checkbox = $(checkboxElement);
+
+		const line_data = checkbox.data('line');
+		block.push(line_data);
+		if ((i === checkboxes.length - 1) || !checkbox.nextAll('.autoreview:first').prop('checked')) {
+			// add block
+			const findCutCount = (cut_count, line_data) => Math.min(cut_count, line_data.indexOf(line_data.trim()));
+			let cut_count = block.reduce(findCutCount, 1000);
+			const addBlockLineToCutCount = line_data => "\n    " + line_data.substr(cut_count);
+			answer_text	+= block.map(addBlockLineToCutCount).join('') + "\n\n---\n";
+			added_lines += block.length;
+			added_blocks++;
+			block.length = 0;  //truncate array
+		}
+		checkbox.prop('checked', false);
+	});
+
+	answer.val(answer_text);
+	$('html, body').animate({
+		scrollTop: answer.offset().top
+	}, 1000);
+	return;
+};
+const checkboxClick = event => {
+	if (event.shiftKey) {
+		const all_checkboxes = $('code input.autoreview');
+		let current_checkbox = $(event.target);
+		const selected = !current_checkbox.prop('checked');
+		do {
+			current_checkbox.prop('checked', !selected);
+			current_checkbox = current_checkbox.prevAll('.autoreview:first');
+		}
+		while (current_checkbox.length === 1 && current_checkbox.prop('checked') == selected);
+	}
+};
+const addCheckboxesToCode = event => {
 	var checkbox;
-	if (clickedObject.data('review')) {
-		var answer = $("#wmd-input");
-		var answer_text = answer.val();
-		var added_lines = 0;
-		var added_blocks = 0;
-		
-		// loop through checkboxes and prepare answer
-		var checkboxes = $("input.autoreview");
-		var block = [];
-		for (i = 0; i < checkboxes.length; i++) {
-			checkbox = $(checkboxes[i]);
-			if (!checkbox.prop('checked')) {
-				continue;
-			}
-			
-			var line_data = checkbox.data('line');
-			block.push(line_data);
-			if ((i === checkboxes.length - 1) || !$(checkboxes[i + 1]).prop('checked')) {
-				// add block
-				var block_line;
-				var cut_count = 1000;
-				for (block_line = 0; block_line < block.length; block_line++) {
-					var cut_this = block[block_line].indexOf(block[block_line].trim());
-					if (cut_count > cut_this) {
-						cut_count = cut_this;
-					}
-				}
-				for (block_line = 0; block_line < block.length; block_line++) {
-					answer_text	+= "\n    " + block[block_line].substr(cut_count);
-				}
-				answer_text	+= "\n\n---\n";
-				added_lines += block.length;
-				added_blocks++;
-				block = [];
-			}
-			checkbox.prop('checked', false);
-		}
-		
-		answer.val(answer_text);
-        $('html, body').animate({
-            scrollTop: answer.offset().top
-        }, 1000);
-		return;
-	}
-	clickedObject.data('review', true);
-	clickedObject.text("Add to answer");
-	
-	var spans = $("code span", clickedObject.next());
-	
-	var count = spans.length;
-	var line = "";
-	var first = null;
-	
-	var checkboxClick = function(e) {
-		if (e.shiftKey) {
-			var all_checkboxes = $('code checkbox.autoreview');
-			var current_checkbox = $(this);
-			var selected = !current_checkbox.prop('checked');
-			do {
-				current_checkbox.prop('checked', !selected);
-				current_checkbox = current_checkbox.prevAll('.autoreview:first');
-			}
-			while (current_checkbox.length === 1 && current_checkbox.prop('checked') == selected);
-		}
-	}
-	
-	for (i = 0; i < count; i++) {
-		var element = $(spans[i]);
-		
+	const clickedButton = $(event.target);
+	clickedButton.text("Add to answer")
+		.toggleClass('addCheckedLinesToAnswer addCheckboxes');
+
+	const spans = $("code span", clickedButton.next());
+	let line = "";
+	let first = null;
+
+	spans.each((i, span) => {
+		let element = $(span);
+
 		if (first === null) {
 			first = element;
 		}
 		if (element.text().indexOf("\n") !== -1) {
-			var lines = element.text().split("\n");
+			let lines = element.text().split("\n");
 			element.text("");
-			for (var line_index = 1; line_index < lines.length; line_index++) {
-				var current_line = lines[line_index];
-				var prev_line = lines[line_index - 1];
-				
-				var span;
+			for (let line_index = 1; line_index < lines.length; line_index++) {
+				let current_line = lines[line_index];
+				const prev_line = lines[line_index - 1];
+
+				let span;
 				// Add the last part of the previous line
 				if (line_index == 1) {
 					line += prev_line;
@@ -117,16 +95,15 @@ embedFunction('showAutoreviewButtons', function(clickedObject) {
 					element.after(span);
 					element = span;
 				}
-				
+
 				// Add the checkbox for the previous line
 				if (line.length > 0) {
 					checkbox = $('<input type="checkbox" class="autoreview"></input>');
 					first.before(checkbox);
 					checkbox.data('line', line);
-					checkbox.click(checkboxClick);
 					first = null;
 				}
-				
+
 				// Add the beginning <span> element for the current line
 				if (line_index < lines.length - 1) {
 					current_line += "\n";
@@ -141,13 +118,18 @@ embedFunction('showAutoreviewButtons', function(clickedObject) {
 		else {
 			line += element.text();
 		}
-	}
+	});
 	if (line.length > 0) {
 		checkbox = $('<input type="checkbox" class="autoreview"></input>');
 		first.before(checkbox);
 		checkbox.data('line', line);
-		checkbox.click(checkboxClick);
 	}
-});
+};
+$(_ => { // jQuery DOM Ready callback
+	console.log('readyEvent');
+	$(document).on('click', '.addCheckedLinesToAnswer', addCheckedLinesToAnswer);
+	$(document).on('click', 'input.autoreview', checkboxClick);
+	$(document).on('click', '.addCheckboxes', addCheckboxesToCode);
 
-$('pre code').parent().before('<a href="javascript:void(0);" onclick="showAutoreviewButtons($(this))">(Review)</a>');
+	$('pre code').parent().before('<button class="addCheckboxes s-btn s-btn__primary">Review</a>');
+});
